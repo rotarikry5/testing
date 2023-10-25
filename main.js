4a3098f988b66226a60f1703009fc7ce1315e7a8
rotarikry5 <cristian.rotari09@mail.ru (http://cristian.rotari09@mail.ru/)>
2023-10-25 15:40:33 +0300

    pereobrazovanie

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log('6 Преобразование типов и базовые операторы')
+console.log('2' + 3);
+console.log(23 + 'mango');
+console.log(true + true);
+const a = 2;
+const b = '3';
+console.log(a + b); // 2 + '3'
+console.log(Number(a) + Number(b)); // 5
+console.log('2²= ' + 2 ** 2 ); // 2² = 4
+console.log('2³= ' + 2 ** 3 ); // 2³ = 8
+console.log('2⁴= ' + 2 ** 4 ); // 2⁴ = 16
+let counter = 2;
+counter++; // работает как counter = counter + 1, просто запись короче
+console.log( counter ); // 3
+let counter = 2;
+counter--; // работает как counter = counter - 1, просто запись короче
+console.log( counter ); // 1
